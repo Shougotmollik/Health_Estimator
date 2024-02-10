0dac61c5104e492e4a661a6aa817f67c775723e1
Shougot mollik <shougotmollik@gmail.com>
2024-02-10 22:48:37 +0600

    start new project

---
+++ lib/utils/theme.dart
@@ -0,0 +1,28 @@
+import 'package:bmi_calculator/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+// ? Light Theme configuration
+var lightTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: const ColorScheme.light(
+    primary: lPrimaryColor,
+    background: lBgColor,
+    onBackground: lFontColor,
+    primaryContainer: lDivColor,
+    onPrimaryContainer: lFontColor,
+    onSecondaryContainer: lLabelColor,
+  ),
+);
+
+// ? Dark Theme configuration
+var darkTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: const ColorScheme.dark(
+    primary: dPrimaryColor,
+    background: dBgColor,
+    onBackground: dFontColor,
+    primaryContainer: dDivColor,
+    onPrimaryContainer: dFontColor,
+    onSecondaryContainer: dLabelColor,
+  ),
+);
