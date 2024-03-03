f74456d682f7c71433c5d1e68bac7ea784dc7779
Shougot mollik <shougotmollik@gmail.com>
2024-03-04 00:37:45 +0600

    Home Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:bmi_calculator/pages/drawer_page.dart';
+import 'package:bmi_calculator/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,10 +7,29 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Message',
+          style: TextStyle(
+            color: Colors.black54,
+          ),
+        ),
+        backgroundColor: Colors.redAccent,
+        toolbarHeight: 50.0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.person),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings),
+          ),
+        ],
       ),
+      body: const LoginPage(),
+      drawer: const MyDrawer(),
     );
   }
 }
