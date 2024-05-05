eb4cc5c21316bc02b7c33ed3f2dbe0cfda4aa39a
Shougot mollik <shougotmollik@gmail.com>
2024-05-05 21:54:50 +0600

    assets

--- lib/Utils/Config/app_images.dart
+++ lib/Utils/Config/app_images.dart
@@ -1,4 +1,6 @@
 class AppImageStrings {
   AppImageStrings._();
   static const String heartbeatImage = "assets/Images/heartbeat.gif";
+  static const String maleAvatarImage = "assets/Images/male_avatar.png";
+  static const String femaleAvatarImage = "assets/Images/female_avatar.png";
 }
